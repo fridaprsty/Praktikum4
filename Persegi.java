3dbc020b35166ca3f84e1c7da2f829e90e5adc2c
farida prsetya <faridaprsty16@gmail.com>
2022-11-12 00:21:04 +0700

    Menambahkan file Persegi.java

---
+++ Persegi.java
@@ -0,0 +1,33 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package utama;
+
+/**
+ *
+ * @author Farida Prasetyaning
+ */
+public class Persegi extends BangunDatar {
+    
+    //buat variabel
+    
+    int sisi;
+    
+    public Persegi (int sisi){
+        this.sisi= sisi ;
+        
+    }
+    
+    @Override
+    public float luas(){
+        return this.sisi = this.sisi;
+    }
+    
+    @Override
+    public float keliling(){
+        return this.sisi * 4;
+    }
+
+}
