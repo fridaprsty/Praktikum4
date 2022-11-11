9c2661a89cd2e71aef10fc3603649334a9225beb
farida prsetya <faridaprsty16@gmail.com>
2022-11-12 00:19:07 +0700

    Menambahkan file BangunDatar.java

---
+++ BangunDatar.java
@@ -0,0 +1,24 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package utama;
+
+/**
+ *
+ * @author Farida Prasetyaning
+ */
+public class BangunDatar {
+    
+    
+    float luas(){
+        System.out.println("Menghitung luas bangun datar");
+        return 0;
+    }
+    float keliling(){
+        System.out.println("Menghitung keliling bangun datar");
+        return 0;
+    }
+
+}
